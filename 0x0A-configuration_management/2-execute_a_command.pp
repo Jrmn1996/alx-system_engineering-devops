b0e5be57f36c95e9cf1559c39ac972403db7d443
Jrmn1996 <jrmn1996@gmail.com>
2023-12-22 09:24:44 -0800

    Execute a command

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
 # manifest that kills a process named killmenow
 exec { 'pkill':
-  comand   => 'pkill killmenow' ,
-  provider => 'shell' ,
+  comand   => 'pkill killmenow',
+  provider => 'shell',
 }
