e17acd0d74152a06719eb2f9fb51b17ebcb5ed4f
Jrmn1996 <jrmn1996@gmail.com>
2024-01-04 15:39:50 -0800

    100

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# make changes to our configuration file
+file_line { 'Turn off password auth':
+  ensure => 'present',
+    path => '/etc/ssh/ssh_config',
+    line => '    PasswordAuthentication no',
+  match  => '#     PasswordAuthentication yes',
+}
+file_line { 'Declare identity file':
+  ensure => 'present',
+    path => '/etc/ssh/ssh_config',
+    line => '    IdentityFile ~/.ssh/holberton',
+}
